e84faa915920dd0bf70e0384b1f10aee387a96fb
Natã A.Rodrigues <nata26ar@gmail.com>
2022-11-07 23:10:24 -0300

    Voltando ao portifólio para buscar emprego

---
+++ script.js
@@ -0,0 +1,20 @@
+const qualityArray = [
+  'Paciente;',
+  'Facilidade de aprendizagem;',
+  'Fácil convivência e relacionamento;',
+  'Disposto a ouvir e aplicar feedbacks e novas ideias;',
+  'Facilidade em mediar discuções de grupo;',
+  'Bom humor e tranquilidade constante;',
+];
+
+let qualList = document.getElementById('qualityList');
+
+const addQualities = (qualities) => {
+  qualities.forEach((qual) => {
+    let createdQual = document.createElement('li');
+    createdQual.innerText = qual;
+    qualList.appendChild(createdQual);
+  });
+};
+
+addQualities(qualityArray);
